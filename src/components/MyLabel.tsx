2993903d818e45e5569914098053cbdfd5908380
Oscar Monroy <oscarmonroy657@gmail.com>
2022-07-04 16:57:16 -0600

    The differents options for my component have been added.

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -6,14 +6,28 @@ interface Props {
    */
   label: string;
   /**
-   * This property set the size of the element
+   * This property set the element size, by default normal is selected.
    */
   size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * This property set the element color. By default primary is selected.
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * This property set if the label will be displayed with uppercase option, by default is false.
+   */
+  allCaps?: boolean;
 }
 
 export const MyLabel = ({
   label = "Default label",
   size = "normal",
+  color = "primary",
+  allCaps = false,
 }: Props) => {
-  return <span className={`${size}`}>{label}</span>;
+  return (
+    <span className={`label ${size} text-${color}`}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
 };
